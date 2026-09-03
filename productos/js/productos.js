cc870c91f19b31e0787a866ed71eba05b3c9e674
Andrea Gonzalez <sofiagonz46@gmail.com>
2026-09-03 02:02:37 -0600

    Actualizar API de productos y filtros

--- productos/js/productos.js
+++ productos/js/productos.js
@@ -1,46 +1,92 @@
-const API_URL = "https://fakestoreapi.com/products";
+const API_URL = "https://dummyjson.com/products";
 
+// Botón para cargar los productos
+const loadProductsButton = document.getElementById("load-products");
+loadProductsButton.addEventListener("click", loadProducts);
+
+// Input para buscar por nombre
+const nameFilter = document.getElementById("name-filter");
+nameFilter.addEventListener("input", applyFilters);
+
+// Contenedor de productos
 const productsContainer = document.getElementById("products-container");
 
-function createProductCard(product) {
+// Contador de productos
+const productsCounter = document.getElementById("products-counter");
+
+// Productos obtenidos de la API
+let _products = [];
+
+// Cargar productos desde la API
+async function loadProducts() {
+  try {
+    const productsData = await fetch(API_URL);
+    const productsJsonData = await productsData.json();
+
+    console.log(productsJsonData);
+    console.log("Cantidad de productos:", productsJsonData.products.length);
+
+    _products = productsJsonData.products;
+
+    displayProducts(_products);
+  } catch (error) {
+    console.log("Error al cargar los productos:", error);
+  }
+}
+
+// Mostrar cards
+function displayProducts(productsToDisplay) {
+  productsContainer.innerHTML = "";
+
+  productsCounter.textContent =
+    `Productos encontrados: ${productsToDisplay.length}`;
+
+  productsToDisplay.forEach((product) => {
     const card = document.createElement("article");
     card.classList.add("product-card");
 
     card.innerHTML = `
-        <img
-            src="${product.image}"
-            alt="${product.title}"
-        >
-        <h2>${product.title}</h2>
-
-        <p>
-            $${product.price}
-        </p>
+      <h4>ID: ${product.id}</h4>
+      <h3>${product.title}</h3>
+      <p>${product.description}</p>
+      <p class="product-price">
+        Precio: $${product.price.toFixed(2)}
+      </p>
+      <p>Rating: ${product.rating}</p>
     `;
 
-    card.addEventListener(
-        "click",
-        () => {
-            window.location.href = `producto-detail.html?id=${product.id}`;
-        });
-
-    return card;
+    productsContainer.appendChild(card);
+  });
 }
 
-async function getProducts() {
+// Aplicar filtro por nombre
+function applyFilters() {
+  const inputValue = nameFilter.value.toLowerCase().trim();
 
-    const response = await fetch(API_URL);
+  // OPCIÓN 1: FOREACH
 
-    const data = await response.json();
+  const newProductsList = [];
 
-    console.log("Respuesta de la API:", data);
+  _products.forEach((product) => {
+    const productName = product.title.toLowerCase();
 
-    productsContainer.innerHTML = "";
+    if (productName.includes(inputValue)) {
+      newProductsList.push(product);
+    }
+  });
 
-    data.forEach(product => {
-        const card = createProductCard(product);
-        productsContainer.appendChild(card);
-    });
+  console.log("Resultado usando foreach:", newProductsList);
+
+  displayProducts(newProductsList);
+
+  // OPCIÓN 2: FILTER + INCLUDES
+  /*
+  const newProductsList = _products.filter((product) =>
+    product.title.toLowerCase().includes(inputValue)
+  );
+
+  console.log("Resultado usando filter e includes:", newProductsList);
+
+  displayProducts(newProductsList);
+  */
 }
-
-getProducts();
